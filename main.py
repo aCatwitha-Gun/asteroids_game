253b6cf9d9ecbb615a72440be6b52ae8d2f0d914
aCatwitha-Gun <noahmaynard96@gmail.com>
2025-08-13 19:59:43 -0400

    fixed game crash on startup caused by asteroid class

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
 from constants import *
 from player import Player
-from asteroid import Asteroids
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     # initialize all of pygame
@@ -11,9 +12,13 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
-    Asteroids.containers = (updatable, drawable)
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
