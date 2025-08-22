f776da7260220e1fa27b5d881b7d0b4511a2c119
Noah <cat@FelineArsenal.localdomain>
2025-08-22 14:55:36 -0400

    Moved game loop to its own file. Extended death timer to make animation longer

--- gameloop.py
+++ gameloop.py
@@ -4,25 +4,6 @@ from player import Player
 from shot import Shot
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
-from title import start_screen
-from gameloop import game_loop
-
-def main():
-    # initialize all of pygame
-    pygame.init()
-    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
-
-    game_state = "START_SCREEN"
-
-    while game_state != "QUIT":
-        if game_state == "START_SCREEN":
-            game_state = start_screen(screen, dt)
-        elif game_state == "PLAYING":
-            game_state = game_loop(screen, dt)
-
-
 
 def game_loop(screen, dt):
     updatable = pygame.sprite.Group()
@@ -77,6 +58,3 @@ def game_loop(screen, dt):
 
         # return delta time and limit framerate to 60 FPS
         dt = clock.tick(60) / 1000
-
-if __name__ == "__main__":
-    main()
